bc6b8772a74cd3f968dbca229d40213ab3230b93
zx <zxpstudy@163.com>
2020-03-26 17:47:02 +0800

    feat: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: "production",
 
@@ -28,6 +30,10 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+      new HtmlWebpackPlugin(),
+  ],
 
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.

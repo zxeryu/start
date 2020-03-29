582999618b74a605fed915724bcce05e1f86787d
zhao xi <zxpstudy@163.com>
2020-03-29 21:25:49 +0800

    chore: eslint config

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 export interface HelloProps {
   compiler: string;
   framework: string;
 }
 
-export const Hello = (props: HelloProps) => (
-  <h1>
-    Hello from {props.compiler} and {props.framework}!
-  </h1>
-);
+export const Hello = (props: HelloProps) => {
+  const handleClick = useCallback(() => {
+    console.log("@@@@@@@@@@@@@");
+  }, []);
+
+  return (
+    <h1 onClick={handleClick}>
+      Hello from {props.compiler} and {props.framework}!
+    </h1>
+  );
+};

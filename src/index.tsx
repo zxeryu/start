0a1afe8971811f27f4a90e496c771b9473881501
zhao xi <zxpstudy@163.com>
2020-04-19 20:56:09 +0800

    chore: delete react demo

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { Hello } from "./components/Hello";
-
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById("root")
-);
